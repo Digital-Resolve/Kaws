4bc35a4b65689138db36bd5eb902ccc268160cce
Sanele Langa <sanele@digitalresolve.co.za>
2023-08-29 22:01:40 +0200

    Start 3d

--- app/product/product.js
+++ app/product/product.js
@@ -3,12 +3,17 @@ import { View, Text, Image } from 'react-native';
 import CommonHeader from "../../components/common/header/CommonHeader";
 import styles from './product.style'
 
+
+
+
+
 const Product = () => {
+    
     return(
     <>
         <CommonHeader />
         <View style={styles.imageContainer}>
-            <Text>Image</Text>
+        
         </View>
 
         <View>
